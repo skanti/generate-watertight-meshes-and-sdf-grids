d7ee89989ff64aa65842b24341b3c03d10959467
Armen Avetisyan <Armen Avetisyan>
2020-08-17 21:35:23 +0200

    More pics.

--- example.sh
+++ example.sh
@@ -1,2 +1,4 @@
 ./build/watertight --in ./data/sofa0.obj --out ./data/sofa0.vox2
 ./build/watertight --in ./data/sofa1.obj --out ./data/sofa1.vox2
+./build/watertight --in ./data/chair0.obj --out ./data/chair0.vox2
+./build/watertight --in ./data/chair1.obj --out ./data/chair1.vox2
